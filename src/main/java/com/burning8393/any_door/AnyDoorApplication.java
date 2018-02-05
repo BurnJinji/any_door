1653f3ca0e767998d3cdd5ec2a0ed66416b4bfc9
Burning8393 <pang798763716@outlook.com>
2018-02-05 15:01:34 +0800

    add profile configure file and mybatis generator plugin then generate orm object.

--- src/main/java/com/burning8393/any_door/AnyDoorApplication.java
+++ src/main/java/com/burning8393/any_door/AnyDoorApplication.java
@@ -1,6 +1,9 @@
 package com.burning8393.any_door;
 
+import com.burning8393.any_door.domain.Book;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @MapperScan("com.burning8393.any_door.mapper")
 public class AnyDoorApplication {
+    @Autowired
+    private Book book;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnyDoorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnyDoorApplication.class, args);
+    }
 
-	@RequestMapping(value = "/", produces = "text/plain;charset=UTF-8")
+    @RequestMapping(value = "/", produces = "text/plain;charset=UTF-8")
     public String index() {
-        return "hello any_door";
+        return "Hello Spring Boot! The BookName is " + book.getName() + ";and Book Author is " + book.getAuthor() + ";and Book Price is " + book.getPrice();
     }
 }
